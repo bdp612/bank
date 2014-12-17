bbab5004dea85e4c30c1d72280454abc6f6378d8
Braedan Purcell <braedanpurcell@gmail.com>
2014-12-17 12:55:19 -0500

    Create app.java

---
+++ app.java
@@ -0,0 +1,17 @@
+package lab5;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import banking.userInterface;
+
+public class app{
+
+	public static void main(String[] args) throws Exception {
+				
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		userInterface UI = (userInterface) ctx.getBean("UI");
+		UI.init();
+	}
+}
